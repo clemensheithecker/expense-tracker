633e82551cfafc5d6c39c673d572fb743fb02005
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-02-26 17:27:24 +0100

    Format `Date` component

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -3,7 +3,11 @@ import { format, parseISO } from "date-fns";
 const Date = ({ dateString }: { dateString: string }) => {
   const dateObject = parseISO(dateString);
 
-  return <time dateTime={dateString}>{format(dateObject, "MMM d, yyyy")}</time>;
+  return (
+    <time dateTime={dateString} className="whitespace-nowrap">
+      {format(dateObject, "MMM d, yyyy")}
+    </time>
+  );
 };
 
 export default Date;
