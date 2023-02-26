1abd3190d463acc8dd38bf1e16bcdfcee3ba810a
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-02-26 11:19:48 +0100

    Improve state management for `NewWalletDialog`

--- src/components/NewWalletDialog.tsx
+++ src/components/NewWalletDialog.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PlusIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 import { Button } from "./Buttons";
 import {
   Dialog,
@@ -10,45 +11,53 @@ import {
   DialogTrigger,
 } from "./Dialog";
 
-export const NewWalletDialog = () => (
-  <>
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button intent="secondary" icon={<PlusIcon />} wide>
-          Create wallet
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogTitle>Create wallet</DialogTitle>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-          }}
-        >
-          <fieldset className="mb-4 mt-6 flex items-center gap-6">
-            <label className="w-24 text-right text-neutral-900" htmlFor="name">
-              Name
-            </label>
-            <input
-              type="text"
-              className="block w-full flex-1 border-gray-300 leading-none text-neutral-900 placeholder:text-neutral-400 focus:border-blue-500 focus:ring-blue-500"
-              id="name"
-              name="name"
-              placeholder="Checking, Savings, etc."
-            />
-          </fieldset>
-          <div className="mt-6 flex justify-end space-x-2">
-            <DialogClose>
-              <Button intent="secondary">Cancel</Button>
-            </DialogClose>
-            <DialogClose>
+const wait = () => new Promise((resolve) => setTimeout(resolve, 1000));
+
+export const NewWalletDialog = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button intent="secondary" icon={<PlusIcon />} wide>
+            Create wallet
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle>Create wallet</DialogTitle>
+          <form
+            onSubmit={(event) => {
+              wait().then(() => setOpen(false));
+              event.preventDefault();
+            }}
+          >
+            <fieldset className="mb-4 mt-6 flex items-center gap-6">
+              <label
+                className="w-24 text-right text-neutral-900"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                className="block w-full flex-1 border-gray-300 leading-none text-neutral-900 placeholder:text-neutral-400 focus:border-blue-500 focus:ring-blue-500"
+                id="name"
+                name="name"
+                placeholder="Checking, Savings, etc."
+              />
+            </fieldset>
+            <div className="mt-6 flex justify-end space-x-2">
+              <DialogClose>
+                <Button intent="secondary">Cancel</Button>
+              </DialogClose>
               <Button type="submit">Create</Button>
-            </DialogClose>
-          </div>
-        </form>
-      </DialogContent>
-    </Dialog>
-  </>
-);
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
 
 export default NewWalletDialog;
