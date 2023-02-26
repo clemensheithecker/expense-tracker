7b8d0eaf1d7d83305497b57a3f1abb7345c903cf
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-02-26 11:00:28 +0100

    Update `Dialog` shadow

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -36,7 +36,7 @@ Dialog.Button = DialogButton;
 export const DialogContent = ({ children }: { children: ReactNode }) => (
   <RadixDialog.Portal>
     <RadixDialog.Overlay className="fixed inset-0 bg-neutral-900/40 data-[state=open]:animate-overlayShow" />
-    <RadixDialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] bg-white p-6 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
+    <RadixDialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] bg-white p-6 shadow-xl focus:outline-none data-[state=open]:animate-contentShow">
       {children}
     </RadixDialog.Content>
   </RadixDialog.Portal>
