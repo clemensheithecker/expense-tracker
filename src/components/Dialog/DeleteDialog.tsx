f809fc3ba0e1d146d4bddaa363e09d4379d7b70c
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-03-05 22:14:13 +0100

    Add `submitting` state to `DeleteDialog` component

--- src/components/Dialog/DeleteDialog.tsx
+++ src/components/Dialog/DeleteDialog.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { TrashIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 import { Button } from "../Buttons";
 import {
   AlertDialog,
-  AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
@@ -11,35 +11,58 @@ import {
   AlertDialogTrigger,
 } from "./AlertDialog";
 
+const wait = () => new Promise((resolve) => setTimeout(resolve, 1000));
+
 export const DeleteDialog = ({
   entityType,
   entityName,
 }: {
   entityType: string;
   entityName: string;
-}) => (
-  <AlertDialog>
-    <AlertDialogTrigger asChild>
-      <Button intent="secondary" icon={<TrashIcon />} wide>
-        Delete {entityType}
-      </Button>
-    </AlertDialogTrigger>
-    <AlertDialogContent>
-      <AlertDialogTitle>Delete {entityType}?</AlertDialogTitle>
-      <AlertDialogDescription>
-        Are you sure you want to delete <strong>{entityName}</strong>{" "}
-        {entityType}? This action cannot be undone.
-      </AlertDialogDescription>
-      <div className="mt-6 flex justify-end space-x-2">
-        <AlertDialogCancel asChild>
-          <Button intent="secondary">Cancel</Button>
-        </AlertDialogCancel>
-        <AlertDialogAction asChild>
-          <Button type="submit" intent="danger">
-            Delete
-          </Button>
-        </AlertDialogAction>
-      </div>
-    </AlertDialogContent>
-  </AlertDialog>
-);
+}) => {
+  const [open, setOpen] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setSubmitting(true);
+    wait().then(() => {
+      setSubmitting(false);
+      setOpen(false);
+    });
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button intent="secondary" icon={<TrashIcon />} wide>
+          Delete {entityType}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogTitle>Delete {entityType}?</AlertDialogTitle>
+        <AlertDialogDescription>
+          Are you sure you want to delete <strong>{entityName}</strong>{" "}
+          {entityType}? This action cannot be undone.
+        </AlertDialogDescription>
+        <div className="mt-6 flex justify-end space-x-2">
+          <AlertDialogCancel asChild>
+            <Button intent="secondary" disabled={submitting}>
+              Cancel
+            </Button>
+          </AlertDialogCancel>
+          <form onSubmit={handleSubmit}>
+            <Button
+              type="submit"
+              intent="danger"
+              disabled={submitting}
+              loading={submitting}
+            >
+              {submitting ? "Deleting..." : "Delete"}
+            </Button>
+          </form>
+        </div>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
